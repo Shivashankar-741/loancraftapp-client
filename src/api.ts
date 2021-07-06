3848002b7bafd51ecbc6f826a3aa0af407a78586
Shivashankar-741 <shiva1894300@gmail.com>
2021-07-06 21:17:57 +0530

    updated backend end points

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://localhost:4000/api/v1',
 });
+
 
 export enum endPoint {
   SIGNIN = '/users/signin',
